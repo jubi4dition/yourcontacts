a0114f00aabb43d5c143ed04d97545e63cc9f0e9
Jubi Dition <yubi4dition@googlemail.com>
2012-02-20 23:52:39 +0100

    span3 to input-large

--- application/views/login.php
+++ application/views/login.php
@@ -22,8 +22,8 @@
 		<div class="row">
 			<div class="span4">
 				<form class="well" action="<?php echo site_url('site/check_login');?>" method="post" accept-charset="utf-8">
-					<input type="text" class="span3" name="email" placeholder="Email" required maxlength="40"> 
-					<input type="password" class="span3" name="password" placeholder="Password" required maxlength="20">
+					<input type="text" class="input-large" name="email" placeholder="Email" required maxlength="40"> 
+					<input type="password" class="input-large" name="password" placeholder="Password" required maxlength="20">
 					<br>
 					<input type="submit" class="btn btn-primary btn-large" value="Login"/>
 				</form>
