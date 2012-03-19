ef7bd07c9d7367e32a93459db0604ac2c2c3bdb3
Jubi Dition <yubi4dition@googlemail.com>
2012-03-19 23:34:01 +0100

    extract navbar creation

--- application/views/edit.php
+++ application/views/edit.php
@@ -1,20 +1,5 @@
 <? $this->load->view('includes/header'); ?>
-<div class="navbar navbar-fixed">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="brand" href="<?=site_url('site')?>"><img src="<?=base_url('css/img/yourcontacts.png')?>" width="57px"/></a>
-			<ul class="nav">
-				<li><?=anchor('site/add', 'Add')?></li>
-				<li><?=anchor('site/delete', 'Delete')?></li>
-				<li class="active"><?=anchor('site/edit', 'Edit')?></li>
-			</ul>
-			<div class="pull-right">
-				<small class="navbar-text">User: <?=anchor('site/profile', $this->session->userdata('email'))?></small>
-				<a href="<?=site_url('login/logout')?>" class="btn btn-primary"><i class="icon-road icon-white"></i> Logout</a>
-			</div>
-		</div>
-	</div>
-</div>
+<? $this->load->view('includes/navbar', array('active' => "edit")); ?>
 <div class="container">
 	<div class="content">
 		<div class="page-header">
---
+++ application/views/includes/navbar.php
@@ -0,0 +1,16 @@
+<div class="navbar navbar-fixed">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="<?=site_url('site')?>"><img src="<?=base_url('css/img/yourcontacts.png')?>" width="57px"/></a>
+			<ul class="nav">
+				<li <? if($active == "add") echo "class=\"active\""?>><?=anchor('site/add', 'Add')?></li>
+				<li <? if($active == "delete") echo "class=\"active\""?>><?=anchor('site/delete', 'Delete')?></li>
+				<li <? if($active == "edit") echo "class=\"active\""?>><?=anchor('site/edit', 'Edit')?></li>
+			</ul>
+			<div class="pull-right">
+				<small class="navbar-text">User: <?=anchor('site/profile', $this->session->userdata('email'))?> </small>
+				<a href="<?=site_url('login/logout')?>" class="btn btn-primary"><i class="icon-road icon-white"></i> Logout</a>
+			</div>
+		</div>
+	</div>
+</div>
