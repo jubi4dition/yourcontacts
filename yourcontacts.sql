c0ef793d3df44deeb2f9ae7fe3bbdb7d7a20f8ec
Jubi Dition <yubi4dition@googlemail.com>
2012-04-06 14:23:16 +0200

    new sql-file

--- yourcontacts.sql
+++ yourcontacts.sql
@@ -1,4 +1,23 @@
 -- 
+-- Structure for table `admins`
+-- 
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- 
+-- Data for table `admins`
+-- 
+
+INSERT INTO `admins` (`aid`, `name`, `password`) VALUES
+  ('1', 'Admin', 'dc647eb65e6711e155375218212b3964');
+
+-- 
 -- Structure for table `contacts`
 -- 
 
@@ -10,17 +29,23 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `email` varchar(50) NOT NULL,
   `phone` varchar(20) NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 
 -- 
 -- Data for table `contacts`
 -- 
 
 INSERT INTO `contacts` (`cid`, `uid`, `name`, `email`, `phone`) VALUES
-  ('18', '1', 'Codeigniter', 'code@igniter.com', '111111111111111'),
-  ('19', '1', 'Twitter Bootstrap', 'twitter@bootstrap.com', '222222222222222'),
-  ('20', '2', 'Jquery', 'jquery@script.com', '3333333333333'),
-  ('21', '2', 'CSS', 'css@style.com', '44444444444444');
+  ('38', '7', 'contact1', 'contact1@mail.com', '111111111111111'),
+  ('39', '7', 'contact2', 'contact2@mail.com', '222222222222222'),
+  ('40', '7', 'contact3', 'contact3@mail.com', '333333333333333'),
+  ('41', '7', 'contact4', 'contact4@mail.com', '444444444444444'),
+  ('42', '8', 'contact1', 'contact1@mail.com', '111111111111111'),
+  ('43', '8', 'contact2', 'contact2@mail.com', '222222222222222'),
+  ('44', '8', 'contact3', 'contact3@mail.com', '333333333333333'),
+  ('45', '9', 'contact1', 'contact1@mail.com', '111111111111111'),
+  ('46', '9', 'contact2', 'contact2@mail.com', '222222222222222'),
+  ('47', '10', 'contact1', 'contact1@mail.com', '111111111111111');
 
 -- 
 -- Structure for table `users`
@@ -31,14 +56,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
+  `contacts` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=18446744073709551615 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- 
 -- Data for table `users`
 -- 
 
-INSERT INTO `users` (`uid`, `email`, `password`) VALUES
-  ('1', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3'),
-  ('2', 'test@test.com', '098f6bcd4621d373cade4e832627b4f6');
+INSERT INTO `users` (`uid`, `email`, `password`, `contacts`) VALUES
+  ('7', 'user2@mail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '4'),
+  ('8', 'user3@mail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '3'),
+  ('9', 'user4@mail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '2'),
+  ('10', 'user1@mail.com', 'ee11cbb19052e40b07aac0ca060c23ee', '1');
 
