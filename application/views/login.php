539ce755749b3a3d9d39bbad1e57475bc7e8385f
Jubi Dition <yubi4dition@googlemail.com>
2012-04-07 17:47:46 +0200

    Admin-Page button at bottom

--- application/views/login.php
+++ application/views/login.php
@@ -48,4 +48,12 @@
 		</div>
 		<? endif; ?>
 	</div>
-<? $this->load->view('includes/footer'); ?>
+	<footer>
+		<p>&copy; 2012 built with CodeIgniter 2.1.0 and Twitter Bootstrap 2.0.1</p>
+	</footer>
+	<div class="pull-right">
+		<a href="<?=site_url('admin')?>" class="btn btn-danger" style="position:fixed; bottom:0;"><i class="icon-flag icon-white"></i> Admin</a>
+	</div>
+</div><!-- /container -->
+</body>
+</html>
