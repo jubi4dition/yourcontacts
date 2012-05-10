0e871c48d6084bd6b5290628a3b8cc74546c90ec
Jubi Dition <yubi4dition@googlemail.com>
2012-05-10 16:30:35 +0200

    vertical-divider in navbar

--- application/views/includes/admin_navbar.php
+++ application/views/includes/admin_navbar.php
@@ -3,9 +3,13 @@
 		<div class="container">
 			<a class="brand" href="<?=site_url('admin')?>"><img src="<?=base_url('css/img/yourcontacts.png')?>" width="57px"/></a>
 			<ul class="nav">
+				<li class="divider-vertical"></li>
 				<li <? if($active == "add") echo "class=\"active\""?>><?=anchor('admin/add', 'Add')?></li>
+				<li class="divider-vertical"></li>
 				<li <? if($active == "delete") echo "class=\"active\""?>><?=anchor('admin/delete', 'Delete')?></li>
+				<li class="divider-vertical"></li>
 				<li <? if($active == "edit") echo "class=\"active\""?>><?=anchor('admin/edit', 'Edit')?></li>
+				<li class="divider-vertical"></li>
 			</ul>
 			<div class="pull-right">
 				<small class="navbar-text">Admin: <?=anchor('admin/profile', $this->session->userdata('admin'))?> </small>
--- application/views/includes/navbar.php
+++ application/views/includes/navbar.php
@@ -3,9 +3,13 @@
 		<div class="container">
 			<a class="brand" href="<?=site_url('site')?>"><img src="<?=base_url('css/img/yourcontacts.png')?>" width="57px"/></a>
 			<ul class="nav">
+				<li class="divider-vertical"></li>
 				<li <? if($active == "add") echo "class=\"active\""?>><?=anchor('site/add', 'Add')?></li>
+				<li class="divider-vertical"></li>
 				<li <? if($active == "delete") echo "class=\"active\""?>><?=anchor('site/delete', 'Delete')?></li>
+				<li class="divider-vertical"></li>
 				<li <? if($active == "edit") echo "class=\"active\""?>><?=anchor('site/edit', 'Edit')?></li>
+				<li class="divider-vertical"></li>
 			</ul>
 			<div class="pull-right">
 				<small class="navbar-text">User: <?=anchor('site/profile', $this->session->userdata('email'))?> </small>
