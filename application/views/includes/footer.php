52f33ce5096f3f62b246f96cf3ff5b8dcb61b8be
Jubi Dition <yubi4dition@googlemail.com>
2013-03-01 16:09:34 +0100

    Responsive, navbar fix

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
   <footer>
-    <p>&copy; 2012 built with CodeIgniter 2.1.3 and Twitter Bootstrap 2.1.1</p>
+    <p>&copy; 2012 built with CodeIgniter 2.1.3 and Twitter Bootstrap 2.3.0</p>
   </footer>
 </div><!-- /container -->
 </body>
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Your Contacts</title>
-<meta name="description" content="manage your contacts with name, email and phone">
-<meta name="author" content="yubi4dition">
-<!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
-  <!--[if lt IE 9]>
-        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
-<!-- Le styles -->
-<link href="<?=base_url('css/bootstrap.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?=base_url('css/yourcontacts.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?=base_url('css/font-awesome.css'); ?>" rel="stylesheet" type="text/css">
-<link href="<?=base_url('css/tablesorter.css'); ?>" rel="stylesheet" type="text/css">
-<!-- Le fav and touch icons -->
-<link href="<?=base_url('css/ico/favicon.ico'); ?>" rel="shortcut icon">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Contacts</title>
+  <meta name="description" content="manage your contacts with name, email and phone">
+  <meta name="author" content="yubi4dition">
+  <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  <!-- Le styles -->
+  <link href="<?=base_url('css/bootstrap.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?=base_url('css/bootstrap-responsive.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?=base_url('css/yourcontacts.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?=base_url('css/font-awesome.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?=base_url('css/tablesorter.css'); ?>" rel="stylesheet" type="text/css">
+  <!-- Le fav and touch icons -->
+  <link href="<?=base_url('css/ico/favicon.ico'); ?>" rel="shortcut icon">
 </head>
 <body>
