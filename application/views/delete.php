2124d5c69c7978fac22864b47f7c0000121bd068
Jubi Dition <yubi4dition@googlemail.com>
2013-03-05 16:24:34 +0100

    some jquery delete

--- application/views/delete.php
+++ application/views/delete.php
@@ -3,17 +3,17 @@
 <div class="container">
 <div class="content" style="display:none">
   <div class="page-header">
-    <h1>Delete A Contact</h1>
+    <h2>Delete A Contact</h2>
   </div>
   <div class="row">
     <div class="span4">
       <form id="formDelete" class="well" accept-charset="utf-8">
           <select id="formSelect" name="name" class="input-block-level">
           <? foreach($contacts as $contact): ?>
-            <option value="<?=$contact['name']; ?>"><?=$contact['name'];?></option>
+            <option value="<?=$contact['name']; ?>"><?=$contact['name']; ?></option>
           <? endforeach;?>
           </select>
-        <button type="submit" class="btn btn-danger btn-large" data-loading-text="Sending...">
+        <button type="submit" class="btn btn-danger btn-large">
         <i class="icon-trash icon-white"></i> Delete Contact</button>
       </form>
     </div>
@@ -30,39 +30,41 @@
   </div>
 </div>
 <script src="<?=base_url('js/jquery.js'); ?>"></script>
-<script src="<?=base_url('js/bootstrap-button.js'); ?>"></script>
 <script type="text/javascript">
 $(document).ready(function() {
   
-    $('#formDelete').submit(function() {
+  $('#formDelete').submit(function() {
 
-        $('#formDelete button').button('loading');
-        $('#success').hide();
-        $('#error').hide();
+    var form = $(this);
+    form.children('button').prop('disabled', true);
+    $('#success').hide();
+    $('#error').hide();
 
-        var faction = '<?=site_url('site/delete_contact')?>';
-        var fdata = $('#formDelete').serialize();
+    var faction = '<?=site_url('site/delete_contact')?>';
+    var fdata = form.serialize();
 
-        $.post(faction, fdata, function(rdata) {
-            var json = $.parseJSON(rdata);
-            if (json.isSuccessful){
-                $('#successMessage').html(json.message);
-                $('#success').show();
-                ('#formSelect option[value="'+ json.name + '"]').remove();
-            } else {
-                $('#errorMessage').html(json.message);
-                $('#error').show();
-            }
+    $.post(faction, fdata, function(rdata) {
 
-            $('#formDelete button').button('reset');
-        });
-      
-        return false;
+        var json = $.parseJSON(rdata);
+        
+        if (json.isSuccessful) {
+            $('#successMessage').html(json.message);
+            $('#success').show();
+            $('#formSelect option[value="'+ json.name + '"]').remove();
+        } else {
+            $('#errorMessage').html(json.message);
+            $('#error').show();
+        }
+
+        form.children('button').prop('disabled', false);
     });
+  
+    return false;
+  });
 
-    $('#nav-delete').addClass('active');
+  $('#nav-delete').addClass('active');
 
-    $('.content').fadeIn(1000);
+  $('.content').fadeIn(1000);
 });
 </script>
 <? $this->load->view('includes/footer'); ?>
