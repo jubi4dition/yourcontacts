8712b93f95b7bb30ee847f0950b8e1d7357092ef
Jubi Dition <yubi4dition@googlemail.com>
2013-03-11 13:27:02 +0100

    updated signup

--- application/controllers/signup.php
+++ application/controllers/signup.php
@@ -7,9 +7,9 @@ class Signup extends CI_Controller
         $this->load->view('signup');
     }
     
-    public function sign_up()
+    public function check()
     {           
-        sleep(2);
+        sleep(1);
         $this->load->library('form_validation');
         $this->form_validation->set_rules('email', 'Email', 'required|max_length[40]|valid_email');
         $this->form_validation->set_rules('email2', 'Email2', 'required|max_length[40]|valid_email');
@@ -17,45 +17,35 @@ class Signup extends CI_Controller
         $this->form_validation->set_rules('pwd2', 'Password2', 'required|max_length[20]|alpha_numeric');
         
         if ($this->form_validation->run() == FALSE) {
-            $json = json_encode(array(
-                'isSuccessful' => FALSE,
-                'message' => "<strong>Registration</strong> failed! Incorrect input."
-            ));
-            echo $json;
+            $message = "<strong>Registration</strong> failed! Incorrect input!";
+            $this->json_response(FALSE, $message);
         } else {   
             $email = $this->input->post('email');
             $pwd = $this->input->post('pwd');
             
             if ($email != $this->input->post('email2')) {
                 $message = "<strong>Emails</strong> do not match!";
-                $json = json_encode(array(
-                    'isSuccessful' => FALSE,
-                    'message' => $message
-                ));
-                echo $json;
+                $this->json_response(FALSE, $message);
             } elseif ($pwd != $this->input->post('pwd2')) {
                 $message = "<strong>Passwords</strong> do not match!";
-                $json = json_encode(array(
-                    'isSuccessful' => FALSE,
-                    'message' => $message
-                ));
-                echo $json;
-            } elseif($this->contacts_model->add_user($email,  $pwd)) {
+                $this->json_response(FALSE, $message);
+            
+            } elseif ($this->user_model->add($email, $pwd)) {
                 $message = "<strong>Registration</strong> successful!";
-                $json = json_encode(array(
-                    'isSuccessful' => TRUE,
-                    'message' => $message
-                ));
-                echo $json;
+                $this->json_response(TRUE, $message);
             } else {
                 $message = "<strong>Email</strong> already exists!";
-                $json = json_encode(array(
-                    'isSuccessful' => FALSE,
-                    'message' => $message
-                ));
-                echo $json;
+                $this->json_response(FALSE, $message);
             }
         }
-    }//end sign_up
+    }
+
+    private function json_response($successful, $message)
+    {
+        echo json_encode(array(
+            'isSuccessful' => $successful,
+            'message' => $message
+        )); 
+    }
 }
 /*End of file signup.php*/
--- application/views/signup.php
+++ application/views/signup.php
@@ -1,81 +1,71 @@
 <? $this->load->view('includes/header'); ?>
 <? $this->load->view('includes/nli_navbar'); ?>
 <div class="container">
-	<div class="content" style="display:none">
-		<div class="page-header">
-			<h1>Sign Up</h1>
-		</div>
-		<div class="row">
-			<div class="span4">
-				<form id="formSignup" class="well" accept-charset="utf-8">
-					<div class="input-prepend">
-						<span class="add-on"><i class="icon-envelope"></i></span>
-						<input type="text" class="input-large" name="email" placeholder="Your Email" required maxlength="40" autofocus />
-					</div>
-					<div class="input-prepend">
-						<span class="add-on"><i class="icon-envelope"></i></span>
-						<input type="text" class="input-large" name="email2" placeholder="Repeat Email" required maxlength="40" />
-					</div>
-					<div class="input-prepend">
-						<span class="add-on"><i class="icon-lock"></i></span>
-						<input type="password" class="input-large" name="pwd" placeholder="Your Password" required maxlength="20" />
-					</div>
-					<div class="input-prepend">
-						<span class="add-on"><i class="icon-lock"></i></span>
-						<input type="password" class="input-large" name="pwd2" placeholder="Repeat Password" required maxlength="20" />
-					</div>
-					<button type="submit" class="btn btn-primary btn-large" data-loading-text="Sending...">
-					<i class="icon-book icon-white"></i> Sign Up</button>
-				</form>
-			</div>
-		</div>
-		<div id="success" class="row" style="display: none">
-			<div class="span4">
-				<div id="successMessage" class="alert alert-success">
-				<p><strong>Registration</strong> successful!</p>
-				<a href="<?=site_url('login')?>" class="btn btn-success"><i class="icon-arrow-right icon-white"></i> log on now</a>
-				</div>
-			</div>
-		</div>
-		<div id="error" class="row" style="display: none">
-			<div class="span4">
-				<div id="errorMessage" class="alert alert-error"></div>
-			</div>
-		</div>
-	</div>
-	<script src="<?=base_url('js/jquery.js')?>"></script>
-	<script src="<?=base_url('js/bootstrap-button.js')?>"></script>
-	<script type="text/javascript">
-	$(document).ready(function() {
-		
-		$("#formSignup").submit(function(){
-			
-			$("#formSignup button").button('loading');
-			$("#success").hide();
-			$("#error").hide();
-			
-			var faction = "<?=site_url('signup/sign_up')?>";
-			var fdata = $("#formSignup").serialize();
+<div class="content" style="display:none">
+  <div class="page-header">
+    <h2>Sign Up</h2>
+  </div>
+  <div class="row">
+    <div class="span4">
+      <form id="formSignup" class="well" accept-charset="utf-8">
+        <input type="text" class="input-block-level" name="email" value="Your Email" placeholder="Your Email" required maxlength="40" autofocus />
+        <input type="text" class="input-block-level" name="email2" placeholder="Repeat Email" required maxlength="40" />
+        <input type="password" class="input-block-level" name="pwd" placeholder="Your Password" required maxlength="20" />
+        <input type="password" class="input-block-level" name="pwd2" placeholder="Repeat Password" required maxlength="20" />
+        <button type="submit" class="btn btn-primary btn-large">
+        <i class="icon-book icon-white"></i> Sign Up</button>
+      </form>
+    </div>
+  </div>
+  <div id="success" class="row" style="display: none">
+    <div class="span4">
+      <div id="successMessage" class="alert alert-success">
+      <p><strong>Registration</strong> successful!</p>
+      <a href="<?=site_url('login'); ?>" class="btn btn-success"><i class="icon-arrow-right icon-white"></i> log on now</a>
+      </div>
+    </div>
+  </div>
+  <div id="error" class="row" style="display: none">
+    <div class="span4">
+      <div id="errorMessage" class="alert alert-error"></div>
+    </div>
+  </div>
+</div>
+<script src="<?=base_url('js/jquery.js'); ?>"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+  
+  $('#formSignup').submit(function() {
+    
+    var form = $(this);
+    form.children('button').prop('disabled', true);
+    $('#success').hide();
+    $('#error').hide();
+    
+    var faction = "<?=site_url('signup/check'); ?>";
+    var fdata = form.serialize();
 
-			$.post(faction, fdata, function(rdata){
-				var json = jQuery.parseJSON(rdata);
-				if(json.isSuccessful){
-					$("#success").show();
-					$("#formSignup input").val("");
-					$("#formSignup input").blur()
-				}else{
-					$("#errorMessage").html(json.message);
-					$("#error").show();
-					$("#formSignup input[name='email']").select();
-				}
-				
-				$("#formSignup button").button('reset');
-			});
-				
-			return false;
-		});
+    $.post(faction, fdata, function(rdata) {
+      
+      var json = jQuery.parseJSON(rdata);
+      
+      if(json.isSuccessful) {
+          $('#success').show();
+          form.children('input').val('');
+          form.children('input').blur()
+      } else {
+          $('#errorMessage').html(json.message);
+          $('#error').show();
+          form.children('input[name="email"]').select();
+      }
+      
+      form.children('button').prop('disabled', false);
+    });
+      
+    return false;
+  });
 
-		$(".content").fadeIn(1000);
-	});
-	</script>
+  $('.content').fadeIn(1000);
+});
+</script>
 <? $this->load->view('includes/footer'); ?>
