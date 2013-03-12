37c26b535cdee11a867f74fac6aa8562fa9d0c17
Jubi Dition <yubi4dition@googlemail.com>
2013-03-12 16:16:08 +0100

    footertext again

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
   <footer>
-    <p>&copy; 2012 built with CodeIgniter v2.1.3, Bootstrap v2.3.1 and jQuery v1.9.1</p>
+    <p>&copy; 2013 built with CodeIgniter v2.1.3, Bootstrap v2.3.1 and jQuery v1.9.1</p>
   </footer>
 </div><!-- /container -->
 </body>
