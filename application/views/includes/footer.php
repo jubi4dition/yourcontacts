94300c3183de59a33b9d58f6fd35d45ae1e49eff
Jubi Dition <yubi4dition@googlemail.com>
2013-03-12 16:11:48 +0100

    bootstrap-2.3.1

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
   <footer>
-    <p>&copy; 2012 built with CodeIgniter 2.1.3 and Twitter Bootstrap 2.3.0</p>
+    <p>&copy; 2012 built with CodeIgniter 2.1.3 and Twitter Bootstrap 2.3.1</p>
   </footer>
 </div><!-- /container -->
 </body>
